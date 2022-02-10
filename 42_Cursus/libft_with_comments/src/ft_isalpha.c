03ad6876162cd00ba945d441d745d5d1b80fbda4
ademirci120 <99325401+ademirci120@users.noreply.github.com>
2022-02-10 17:41:19 +0300

    Update description

--- 42_Cursus/libft_with_comments/src/ft_isalpha.c
+++ 42_Cursus/libft_with_comments/src/ft_isalpha.c
@@ -10,11 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// ft_isalpha 
+// Girilen karakterin alfabetik bir karakter olup olmadığını kontrol eder.
+// Eğer alfabetik bir karakter ise "1" döndürür.
+// Eğer alfabetik bir karakter değil ise "0" döndürür.
+// Çalışırken hem büyük hem küçük harfi dikkate alır.
+
 int	ft_isalpha(int str)
 {
-	if (str >= 'a' && str <= 'z')
-		return (1);
-	else if (str >= 'A' && str <= 'Z')
-		return (1);
-	return (0);
+	if (str >= 'a' && str <= 'z')			// Eğer girilen değer ASCII değerine göre "a" ile "z" arasında ise
+		return (1);				// "1" döndür	
+	else if (str >= 'A' && str <= 'Z')		// Eğer girilen değer ASCII değerine göre "A" ile "Z" arasında ise
+		return (1);				// "1" döndür
+	return (0);					// "0" döndür
 }
