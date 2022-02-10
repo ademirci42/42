3005d517b118ec9de91cafe8226c29b0f31e79ff
Alper Demirci <ademirci@k2m15s04.42kocaeli.com.tr>
2022-02-10 14:38:20 +0300

    II

---
+++ 42_Cursus/libft/src/ft_strncmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademirci <ademirci@student.42kocaeli.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 14:03:31 by ademirci          #+#    #+#             */
+/*   Updated: 2022/02/10 14:04:56 by ademirci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
+
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (i < n && (str1[i] || str2[i]))
+	{
+		if (str1[i] > str2[i])
+			return (1);
+		else if (str2[i] > str1[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
