7ea62f6028a5df27f6a52738d32bd7af34ebb60c
ademirci120 <99325401+ademirci120@users.noreply.github.com>
2022-02-12 14:41:50 +0300

    Update description

--- 42_Cursus/libft_with_comments/src/ft_memset.c
+++ 42_Cursus/libft_with_comments/src/ft_memset.c
@@ -9,6 +9,15 @@
 /*   Updated: 2022/02/01 17:01:42 by ademirci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// ft_memset
+// Girilen stringin sansürlenmesini sağlar.
+// Fonksiyona sansürleme yaparken kullanılacak karakter ve soldan sağa kaç karakterin sansürleneceğinin girilmesi gerekir.
+// Sansürlenmiş stringi geri döndürür.
+// void *b    => String.
+// int c      => Sansürlenirken kullanılacak karakter.
+// size_t len => Sansürlenecek karakter sayısı (soldan sağa).
+
 #include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
@@ -17,11 +26,11 @@ void	*ft_memset(void *b, int c, size_t len)
 	char	*str;
 
 	i = 0;
-	str = (char *)b;
-	while (i < len)
+	str = (char *)b;				// str pointer'ına b'yi takip ettirir.
+	while (i < len)					// Sansürlenecek karakter sayısı i'den büyük olana kadar.
 	{
-		str[i] = c;
-		i++;
+		str[i] = c;				// str'nin "i" değerine "c" yi yaz.
+		i++;					// "i" yi arttır.
 	}
-	return (str);
+	return (str);					// "str" yi döndür.
 }
