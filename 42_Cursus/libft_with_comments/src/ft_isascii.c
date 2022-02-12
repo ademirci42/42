9c7ca490175402b80a2b79d0de81d3aba1b50593
ademirci120 <99325401+ademirci120@users.noreply.github.com>
2022-02-12 14:11:49 +0300

    Update description

--- 42_Cursus/libft_with_comments/src/ft_isascii.c
+++ 42_Cursus/libft_with_comments/src/ft_isascii.c
@@ -10,9 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// ft_isascii
+// Girilen karakterin ASCII tablosunda bir karakter olup olmadığını kontrol eder.
+// Eğer ASCII tablosunda bir karakter ise "1" döndürür.
+// Eğer aASCII tablosunda bir karakter değil ise "0" döndürür.
+
 int	ft_isascii(int str)
 {
-	if (str >= 0 && str < 127)
-		return (1);
-	return (0);
+	if (str >= 0 && str < 127)			// Eğer girilen değer ASCII değerine göre "0" ile "127" arasında ise
+		return (1);				// "1" döndür
+	return (0);					// "0" döndür
 }	
