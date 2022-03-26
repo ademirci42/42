dd1f21af21db2f510facb662ea91f70d94d66683
ademirci42 <99325401+ademirci42@users.noreply.github.com>
2022-03-26 23:07:46 +0300

    update comments

--- 42_Cursus/libft_with_comments/ft_isalnum.c
+++ 42_Cursus/libft_with_comments/ft_isalnum.c
@@ -10,10 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// ft_isalnum
+// Girilen karakterin alfanumerik bir karakter olup olmadığını kontrol eder.
+// Eğer alfanumerik bir karakter ise "1" döndürür.
+// Eğer alfanumerik bir karakter değil ise "0" döndürür.
+// Çalışırken hem büyük hem küçük harfi dikkate alır.
+
 int	ft_isalnum(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
-		|| (c >= '0' && c <= '9'))
-		return (1);
-	return (0);
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')			// Eğer girilen değer ASCII değerine göre "0" ile "9" arasında			
+		|| (c >= '0' && c <= '9'))					// veya "a" ile "z" arasında veya "A" ile "Z" arasında ise
+		return (1);							// "1" döndür
+	return (0);								// "0" döndür
 }
